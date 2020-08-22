d19677233d11bf3402d8479a21a5a4bb3631eea5
Richard Kurwitz <kurwitz3@aol.com>
2020-08-22 19:42:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,9 +48,9 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season,holiday|
     puts  "#{season.capitalize}:"
     holiday.each do |key,value|
-      if key == :christmas
+      if
       puts  "  #{key.to_s.capitalize}: #{ value.join(", ")}"
-      elsif key == :new_years
+    
       puts "  #{key.to_s.split("_").collect{|list|list.capitalize}.join(" ")}: #{ value.join(", ")}"
   end
   end
