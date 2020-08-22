0e9d36e20975ed867a374c29f5889a9949dea92f
Richard Kurwitz <kurwitz3@aol.com>
2020-08-22 19:42:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,11 +48,10 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season,holiday|
     puts  "#{season.capitalize}:"
     holiday.each do |key,value|
-      if
-      puts  "  #{key.to_s.capitalize}: #{ value.join(", ")}"
+      
     
       puts "  #{key.to_s.split("_").collect{|list|list.capitalize}.join(" ")}: #{ value.join(", ")}"
-  end
+  
   end
     end
    
